bb6223a100641a156facde3bc068faac45d3dadb
kaylawoodbury <kaylasandoval92@gmail.com>
2020-02-09 18:44:07 +0100

    attempting to update checkout process, still need additional updates to function

--- lib/library.rb
+++ lib/library.rb
@@ -1,5 +1,7 @@
 require 'yaml'
 require 'date'
+require 'pp'
+
 
 class Library
     attr_accessor :catalog, :return_date, :books_checked_out, :user_search, :books_returned
@@ -13,61 +15,67 @@ class Library
         @books_checked_out = []
     end
 
-    def full_catalog
-        
-    end 
 
+    #guides users to available actions, working as expected
     def start
         puts "Welcome to the Digital Library"
         puts "Are you a visitor or employee? "
         user_type = gets.chomp.to_s
         if user_type == 'visitor'
-            puts "Available actions include:"
-            puts 'library.full_catalog'
-            puts "library.title_search"
-            puts "library.author_search"
-            puts "library.book_to_checkout"
-            puts "library.book_to_return"
+            puts 'Available actions include:'
+            puts 'library.catalog'
+            puts 'library.title_search'
+            puts 'library.author_search'
+            puts 'library.check_out_book'
+            puts 'library.return_book'
         else
-            puts "To view full library catalog: library.full_catalog"
-            puts "To add new book to library collection: library.add_new_book"
+            puts 'Available actions include:'
+            puts 'library.catalog'
+            puts 'library.add_new_book'
         end
     end
 
-   #User can search
+   #User can search, working as expected
     def author_search
         puts 'Search for books with author name like: '
         author_name = gets.chomp.to_s
-        @book = catalog.select { |obj| obj[:item][:author].include? author_name }
+        pp catalog.select { |obj| obj[:item][:author].include? author_name }
+        
     end
 
     def title_search
         puts 'Search for books with title like: '
         book_title = gets.chomp.to_s
-        @book = catalog.select { |obj| obj[:item][:title].include? book_title}
+        #@book = catalog.select { |obj| obj[:item][:title].include? book_title}
+        pp catalog.select { |obj| obj[:item][:title].include? book_title}
+
     end
 
-    
+####### Check Out Book ################
+    #Not working
     # we then want to check the book's availability
     # we then want an OR statement to either checkout book or say book is not available (maybe do in one step?)
-    def book_to_checkout
+    def check_out_book
         puts 'Enter full title of book you wish to checkout: '
         full_book_title = gets.chomp.to_s
-        availablity = book_to_checkout(user_search)[0][:available]
+        is_book_available = {}
+        is_book_available << @catalog.detect{ |obj| obj[:item][:title] == full_book_title }
+        availablity = is_book_available[:available]
+        #availablity = @book[0][:available]
         if availability == true
             puts 'This book is available, would you like to continue to checkout? (yes/no)'
-            proceed_to_checkout = gets.chomp.to_s
-            if proceed_to_checkout == yes
-                books_checked_out << @catalog.detect { |obj| obj[:item][:title] == full_book_title }
-                checkout()
-            else  puts 'Checkout cancelled'
-            end
+            #proceed_to_checkout = gets.chomp.to_s
+            #if proceed_to_checkout == yes
+                #books_checked_out << @catalog.detect { |obj| obj[:item][:title] == full_book_title }
+                #checkout()
+            #else  puts 'Checkout cancelled'
+            #end
         else puts 'This book is currently unavailable'
         end
     end
      
     # then if the book is available we want to 'checkout' by updating the books availability and return_date and message to user
-    def checkout
+    def checkout()
         #update availability
         book_to_checkout(user_search)[0][:available] = false
         #update return date
@@ -84,7 +92,9 @@ class Library
         Date.today.next_month(STANDARD_TIME_MONTH).strftime('%D')
     end   
     
-    def book_to_return
+    
+########## Return Books ################
+    def return_book
         puts 'Enter full title of book you wish to return: '
         full_book_title = gets.chomp.to_s
         @books_returned << @catalog.detect { |obj| obj[:item][:title] == full_book_title}
@@ -106,6 +116,7 @@ class Library
         @books_returned = nil
     end
 
+###### Add new book #########
     #Still working on this, does not yet add to yml file
     def add_new_book
         print "Title of New Book: "
@@ -114,6 +125,4 @@ class Library
         new_book_author = gets.chomp.to_s
         File.open('./lib/data.yml', 'w') { |f| f.write catalog.to_yaml } <<{:item=>{:title=>new_book_title, :author=>new_book_author}, :available=>true, :return_date=>nil}
     end
-
-
 end
